d317409587ea44ee982bad05b1be99a2c5350257
Dobry Nikolov <dobry989@gmail.com>
2021-04-06 13:32:57 +0300

    Solved the first problem from the lab document

--- Regular Expressions/01. Match Full Name.py
+++ Regular Expressions/01. Match Full Name.py
@@ -1,2 +1,6 @@
-verakrjmaskldxfhgdfghdfh
-asdasdasd
+import re
+
+names = input()
+regex_pattern = "\\b[A-Z][a-z]+ \\b[A-Z][a-z]+\\b"
+matches = " ".join(re.findall(regex_pattern, names))
+print(matches)
