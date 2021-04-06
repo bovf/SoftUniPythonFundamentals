e577766328c28f0a8647e0ca707bd44da4d0565f
Dobry Nikolov <dobry989@gmail.com>
2021-04-06 14:23:16 +0300

    Missed a )

--- Regular Expressions/02. Match Phone Number.py
+++ Regular Expressions/02. Match Phone Number.py
@@ -1,6 +1,6 @@
 import re
 
-number_pattern = "(\+359-2-[0-9]{3}-[0-9]{4}|\+359 2 [0-9]{3} [0-9]{4})\\b"
+number_pattern = "(\+359-2-[0-9]{3}-[0-9]{4}|\+359 2 [0-9]{3} [0-9]{4})\\b)"
 
 telephone_numbers = input()
 valid_numbers = re.findall(number_pattern, telephone_numbers)
